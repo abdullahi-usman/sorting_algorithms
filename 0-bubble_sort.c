ed8618d09ec1287068bf8961adf91fcabe8ea6f6
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-24 04:36:40 +0100

    fix indent

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,20 +6,23 @@
  * bubble_sort - the bubble sort algorithm
  * @array: the array to sort
  * @size: the size
- * 
  * Return: void
 */
 void bubble_sort(int *array, size_t size)
 {
 	int i, swap_number = 0;
 
-	while(1)
+	if (size < 2)
+		return;
+
+	while (1)
 	{
 		for (i = 0; i < (int)size; i++)
 		{
 			if (i != 0 && (array[i] < array[i - 1]))
 			{
 				int temp = array[i - 1];
+
 				array[i - 1] = array[i];
 				array[i] = temp;
 
@@ -28,11 +31,11 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 
-		
+
 		if (swap_number == 0)
 			break;
-		
+
 		swap_number = 0;
 	}
-		
+
 }
